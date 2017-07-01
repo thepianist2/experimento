80dd1c8f14be08a7ca3c870d222b1bc85f808172
Fabian Andres Allel Gallardo <the_pianist2@hotmail.com>
2017-07-01 12:14:04 +0200

    primera subida, comunicación realizada

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var opener = require('opener');
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(client){
+    client.emit("hola");
+    console.log('Enviando hola desde servidor'+new Date().getSeconds()+':'+new Date().getMilliseconds());
+    client.on("hola servidor", function(){
+        console.log("Recibido hola desde cliente"+new Date().getSeconds()+':'+new Date().getMilliseconds());
+        client.emit("adios cliente");
+        console.log("Enviando adios desde servidor"+new Date().getSeconds()+':'+new Date().getMilliseconds());
+    })
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+opener("http://localhost:3000");
+    
