f41bfe43b272095030a9d0a27707dc5bc41ac7e8
Fabian Allel <fabian.allel@i3television.es>
2017-07-04 15:24:54 +0200

    test.conf.json

--- index.js
+++ index.js
@@ -4,35 +4,55 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var opener = require('opener');
+var colors = require('colors');
+var asyncLoop = require('node-async-loop');
+var resultPattern = [];
 
-var pgk = require(process.cwd()+'/package.json');
-
-console.log(pgk.version);
-
-
-
-console.log("Levantando test!!!");
+var configFile = require(process.cwd()+'/test.conf.json');
 
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+
+//allow paths publics
+configFile.publicPaths.forEach(function(value){
+  app.use(express.static(value));
 });
 
-app.use(express.static('public'));
+//executing test array
+configFile.tests.forEach(function(test){
+  console.log(colors.cyan("OPENING URL: "+test.url.green+"...".green));
+  opener(test.url);
 
-io.on('connection', function(client){
-    client.emit("hola");
-    console.log("Enviando hola desde servidor "+new Date().getSeconds()+':'+new Date().getMilliseconds());
-    client.on("hola servidor", function(){
-        console.log("Recibido hola desde cliente "+new Date().getSeconds()+':'+new Date().getMilliseconds());
-        client.emit("adios cliente");
-        console.log("Enviando adios desde servidor "+new Date().getSeconds()+':'+new Date().getMilliseconds());
-    })
+  console.log(colors.cyan("EXECUTING TEST: "+test.name.green));
+    io.on('connection', function(client){
+      test.scenarios.forEach(function(scenario, index){
+        console.log(colors.cyan("SENDING ACTION TO CLIENT: "+scenario.action.green));
+        client.emit(scenario.action);
+        //listener fromclient only one time
+        if(index===0){
+          //check patterns test
+          client.on("fromclient", function(clase, message){
+            console.log(colors.cyan("RESPONSE RECEIVED: "+message.green));
+            resultPattern.push(message);
+            //all patterns they should be passed
+            if(resultPattern.length===scenario.patterns.length){
+              //compare test result with pattern expect
+              if(resultPattern.every(function(results, i) {return results === scenario.patterns[i].status; })){
+                console.log("Ha pasado correctamente");
+              }else{
+                console.log("Ha Fallado");
+              }
+            }
+          });
+        }
+      });
+    });
+
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+
+http.listen(configFile.portServer, function(){
+  console.log('Test listening in port: '+configFile.portServer);
 });
 
-opener("http://localhost:3000");
+
     
