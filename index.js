8c2c9250e1d82b4fafebd4acf43ea3933f9230db
Fabian Allel <fabian.allel@i3television.es>
2017-07-06 12:17:28 +0200

    falta quitar listener de client.on

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var opener = require('opener');
 var colors = require('colors');
+var indiceTest = 0;
 
 var configFile = require(process.cwd()+'/test.conf.json');
 
@@ -15,9 +16,13 @@ configFile.publicPaths.forEach(function(value){
   app.use(express.static(process.cwd()+'/'+value));
 });
 
-//executing test array
-configFile.tests.forEach(function(test){
-  
+const exit = () => {
+  setTimeout(() => process.exit(), 0);
+};
+
+
+function executeTest(test){
+
   console.log(colors.cyan("OPENING URL: "+test.url.green+"...".green));
   opener(test.url);
 
@@ -28,7 +33,7 @@ configFile.tests.forEach(function(test){
       console.log(colors.cyan("SENDING ACTION TO CLIENT: "+scenario.action.green));
       client.emit(scenario.action);
       //listener fromclient only one time
-      if(index===0){
+      //if(index===0){
         //check patterns test
         client.on("fromclient", function(clase, message){
           console.log(colors.cyan("RESPONSE RECEIVED: "+clase+' '+message.green));
@@ -41,6 +46,10 @@ configFile.tests.forEach(function(test){
               if(filterResult.every(function(results, i) {return results === scenario.patterns[i].status; })){
                 console.log(colors.green("THE TEST \'"+ test.name +"\' PASSED SUCCESSFULLY !!"));
                 resultPattern = [];
+                client.emit('close');
+                client.disconnect(true);
+                //exit();
+                executeNextTest();
               }else{
                 console.log("Result match pattern: \'"+filterResult+"\'");
                 console.log("Waiting pattern: \'"+scenario.patterns.map((pattern)=>{return pattern.status})+"\'");
@@ -48,17 +57,38 @@ configFile.tests.forEach(function(test){
                 //throw new TypeError("THE TEST \'"+ test.name +"\' FAILED !!");
                 console.log(colors.red("THE TEST \'"+ test.name +"\' FAILED !!"));
                 //setTimeout(() => process.exit(), 0);
+                client.emit('close');
+                exit();
+                client.disconnect(true);
+                executeNextTest();
               }
             });
-          }else{
-
           }
         });
-      }
+      //}
     });
+
+
   });
 
-});
+}
+
+
+
+
+
+executeTest(configFile.tests[indiceTest]);
+
+
+function executeNextTest(){
+  indiceTest+=1;
+  if(indiceTest<configFile.tests.length){
+    executeTest(configFile.tests[indiceTest]);
+  }else{
+    console.log("Todos los test han finalizado");
+    exit();
+  }
+}
 
 
 //compare result pattern and filter with the waiting pattern
